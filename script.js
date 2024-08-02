35f6e5e30303cc86c0987dc1893688b71672d97e
dreadpiraterobertts <116425729+dreadpiraterobertts@users.noreply.github.com>
2024-08-02 11:45:06 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateTime() {
+    const now = new Date()
+
+    const newYorkTime = new Date(now.toLocaleString('en-US', {timeZone:'America/New_York'}))
+    document.querySelector('#new-york .time').textContent = newYorkTime.toLocaleTimeString();
+
+    const londonTime = new Date(now.toLocaleString('en-GB', {timeZone:'Europe/London'}))
+    document.querySelector('#london .time').textContent = londonTime.toLocaleTimeString();
+
+    const tokyoTime = new Date(now.toLocaleString('ja-JP', {timeZone:'Asia/Tokyo'}))
+    document.querySelector('#tokyo .time').textContent = tokyoTime.toLocaleTimeString();
+
+
+}
+
+setInterval(updateTime,1000)  //calls the funtion every 1000ms which is 
+updateTime() //initializes the function call for the first time
